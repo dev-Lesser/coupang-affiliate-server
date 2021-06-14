2b65deccb4614372eff6f300bcae8fbcbb9ca99c
dev-Lesser <dev.jongmoon@gmail.com>
2021-06-15 06:26:11 +0900

    :art: Deploy to heroku

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
-from datetime import datetime, timezone, timedelta
+from datetime import datetime,timezone, timedelta
 from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
-import json
 import pandas as pd
 app = FastAPI()
 
@@ -163,13 +162,21 @@ async def get_theme():
 #     )
 
 @app.get("/api/v1.0/data")
-async def get_anaylsis_theme_data(theme:str):
+async def get_anaylsis_theme_data(theme:str, date:str):
     collection = db[env.COLLECTION_ANALYSIS]
-    result = collection.find_one({'theme':theme},{'_id':0})
+    year,month,day = date.split('-')
+    convert_date = datetime(int(year),int(month),int(day),23,59)
+    result = collection.find_one({'theme':theme, 'start_date':{'$lte':convert_date}},{'_id':0}, sort=[('start_date', -1)])
     keyword, bigram, trigram = result['data']['keyword'], result['data']['bigram'], result['data']['trigram']
 
     if result:
-        result['analysis_date'] = result['analysis_date'].strftime('%Y-%m-%d %H:%M:%S')
+        result['start_date'] = result['start_date'].strftime('%Y-%m-%d %H:%M:%S')
+        result['end_date'] = result['end_date'].strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        return JSONResponse(
+        status_code=401,
+        content= None
+    )
 
     df = pd.concat([pd.DataFrame(keyword),pd.DataFrame(bigram),pd.DataFrame(trigram)],axis=1)
     df.columns = ['keyword','keyword_num','bigram','bigram_num','trigram','trigram_num']
@@ -180,112 +187,6 @@ async def get_anaylsis_theme_data(theme:str):
         content= result
     )
 
-@app.get("/api/v1.0/raw_data/{sid}/{eid}")
-def get_raw_data(sid:int=0, eid:int=10,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_MAIN]
-    result = []
-    data = list(collection.find({},{'_id':0}).sort([("add_date", -1)]).skip(sid).limit(eid-sid))
-    for i in data:
-        convert_date = i['add_date'].strftime('%Y-%m-%d %H:%M:%S')
-        result.append({
-            'theme': i['theme'],
-            'blog_id': i['blog_id'],
-            'log_no':i['log_no'],
-            'url':i['url'],
-            'contents': i['contents'],
-            'date': convert_date
-        })
-
-    return JSONResponse(
-        status_code=200,
-        content= result
-    )
-# raw data 삭제
-@app.delete("/api/v1.0/raw_data")
-def delete_raw_data(blog_id: str, log_no: str, Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_MAIN]
-    collection.delete_one(
-        {
-            'blog_id':blog_id,
-            'log_id':log_no
-        })
-
-    return JSONResponse(
-        status_code=200,
-        content= 'Delete Success {}  {}'.format(blog_id, log_no)
-    )
-@app.get("/api/v1.0/blacklist/{sid}/{eid}")
-async def get_blacklist(sid:int=0, eid:int=10,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_BLOCK]
-    data = list(collection.find({},{'_id':0}).sort([("blog", 1)]).skip(sid).limit(eid-sid))
-
-    return JSONResponse(
-        status_code=200,
-        content= data
-    )
-
-@app.post("/api/v1.0/blacklist")
-async def add_blacklist(blacklist: BlackList,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_BLOCK]
-    data = collection.find_one({'blog':blacklist.blog}, upsert=True)
-    if data:
-        return JSONResponse(
-            status_code=202,
-            content= "Already exist"
-        )
-
-    return JSONResponse(
-        status_code=201,
-        content= "Add blacklist success id : {}".format(blacklist.blog)
-    )
-
-@app.delete("/api/v1.0/blacklist")
-async def delete_blacklist(blog: str,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_BLOCK]
-    collection.delete_one({'blog':blog})
-
-    return JSONResponse(
-        status_code=200,
-        content= "Delete blacklist success id : {}".format(blog)
-    )
-
-@app.get("/api/v1.0/stopwords/{sid}/{eid}")
-async def get_stopwords(sid:int=0, eid:int=10,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_STOPWORDS]
-    data = list(collection.find({},{'_id':0}).sort([("stop", 1)]).skip(sid).limit(eid-sid))
-
-    return JSONResponse(
-        status_code=200,
-        content= data
-    )
-
-@app.post("/api/v1.0/stopwords")
-async def add_stopwords(stopwords: StopWords,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_STOPWORDS]
-    collection.insert_one({'stop': stopwords.stop}, upsert=True)
-
-    return JSONResponse(
-        status_code=201,
-        content= "Add success stopwords {}".format(stopwords.stop)
-    )
-
-@app.delete("/api/v1.0/stopwords")
-async def delete_stopwords(stopwords: StopWords,  Authorize: AuthJWT = Depends()): 
-    Authorize.jwt_required()
-    collection = db[env.COLLECTION_STOPWORDS]
-    collection.delete_one({'stop': stopwords.stop})
-
-    return JSONResponse(
-        status_code=200,
-        content= "Delete success stopwords {}".format(stopwords.stop)
-    )
 
 
 if __name__ == '__main__':
